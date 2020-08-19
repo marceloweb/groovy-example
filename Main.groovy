edc9b10957f535adf5b1f44217e8509573567413
Marcelo Oliveira <marcelo@marceloweb.info>
2020-08-19 09:11:20 -0300

    - initial

---
+++ Main.groovy
@@ -0,0 +1,10 @@
+import Student
+
+class Main {
+   static void main(String[] args) {
+      Student mst = new Student();
+      mst.name = "Joe";
+      mst.ID = 1;
+      println(mst.Display())
+   } 
+} 
